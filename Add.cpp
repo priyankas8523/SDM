35b0ddc2dbe15785f0f3f10c2711d9db9d9b33e3
shreyashkilledar <shreyashskilledar24@gmail.com>
2023-09-13 22:43:15 +0530

    Addition Program using CPP

---
+++ Add.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+void main()
+{
+    int a;
+    int b;
+    int add;
+    cout<<"Enter the value of a=";
+    cin>>a;
+    cout<<"Enter the value of b=";
+    cin>>b;
+    add=a+b;
+    cout<<"Addition="<<add;
+    getch();
+}
